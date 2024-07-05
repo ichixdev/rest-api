4d447ab9b72b7476d7adbbeff5906048894fa201
Luis Moncada <luiismoncada@gmail.com>
2024-07-04 23:04:03 -0400

    added MVC

--- schemas/movies.js
+++ schemas/movies.js
@@ -1,16 +1,16 @@
-const z = require('zod');
+import z from 'zod'
 
 const movieSchema = z.object({
   title: z.string({
     invalid_type_error: 'Movie title must be a string',
-    required_error: 'Movie title is required.',
+    required_error: 'Movie title is required.'
   }),
   year: z.number().int().min(1900).max(2024),
   director: z.string(),
   duration: z.number().int().positive(),
   rate: z.number().min(0).max(10).default(5),
   poster: z.string().url({
-    message: 'Poster must be a valid URL',
+    message: 'Poster must be a valid URL'
   }),
   genre: z.array(
     z.enum([
@@ -22,24 +22,19 @@ const movieSchema = z.object({
       'Fantasy',
       'Horror',
       'Thriller',
-      'Sci-Fi',
+      'Sci-Fi'
     ]),
     {
       required_error: 'Movie genre is required.',
-      invalid_type_error: 'Movie genre must be an array of enum Genre',
+      invalid_type_error: 'Movie genre must be an array of enum Genre'
     }
-  ),
-});
+  )
+})
 
-function validateMovie(input) {
-  return movieSchema.safeParse(input);
+export function validateMovie (input) {
+  return movieSchema.safeParse(input)
 }
 
-function validatePartialMovie(input) {
-  return movieSchema.partial().safeParse(input);
+export function validatePartialMovie (input) {
+  return movieSchema.partial().safeParse(input)
 }
-
-module.exports = {
-  validateMovie,
-  validatePartialMovie,
-};
